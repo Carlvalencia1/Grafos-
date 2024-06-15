04ef6d0952d738eca5cc127dd76402871bf56c41
Adrian <233318@ids.upchiapas.edu.mx>
2024-06-15 16:46:11 -0700

    Commit de adrián

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import models.*;
@@ -6,48 +5,52 @@ import models.*;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int opcion, cantidad;
+        int opcion = 0;
+        int cantidad = 0;
         Grafo grafo = new Grafo();
 
         do {
-            System.out.println("Menu");
-            System.out.println("1) Hacer grafo");
-            System.out.println("2) Imprimir grafo");
-            System.out.println("3) Imprimir vertices");
-            System.out.println("4) Salir");
-            System.out.println("Indique la opción que desee elegir");
-            opcion = entrada.nextInt();
+            try {
+                System.out.println("Menu");
+                System.out.println("1) Hacer grafo");
+                System.out.println("2) Imprimir grafo");
+                System.out.println("3) Salir");
+                System.out.print("Indique la opción que desee elegir: ");
+                opcion = entrada.nextInt();
 
-            switch (opcion) {
-                case 1:
-                    System.out.println("Indica la cantidad de vertices del grafo");
-                    cantidad = entrada.nextInt();
-                    for (int i = 0; i < cantidad; i++) {
-                        Vertice nuevoVertice = crearVertice(grafo, entrada);
-                        if (nuevoVertice != null) {
-                            grafo.agregarNodo(nuevoVertice);
+                switch (opcion) {
+                    case 1:
+                        try {
+                            System.out.println("Indica la cantidad de vertices del grafo");
+                            cantidad = entrada.nextInt();
+
+                            for (int i = 0; i < cantidad; i++) {
+                                Vertice nuevoVertice = crearVertice(grafo, entrada);
+                                if (nuevoVertice != null) {
+                                    grafo.agregarNodo(nuevoVertice);
+                                }
+                            }
+                            grafo.agregarAristas();
+                        } catch (Exception e) {
+                            System.out.println("Error: Ingrese datos numéricos");
+                            entrada.next();
                         }
-                    }
-                    grafo.agregarAristas();
-                    break;
-                case 2:
-                    System.out.println(grafo);
-                    break;
-                case 3:
-                    List<Vertice> listaDeVertices = grafo.getVertices();
-                    System.out.println("Los vertices son:");
-                    for (Vertice vertice : listaDeVertices) {
-                        System.out.print(vertice.getDato() + " ");
-                    }
-                    System.out.println();
-                    break;
-                case 4:
-                    System.out.println("Saliendo del programa.");
-                    break;
-                default:
-                    System.out.println("Opción no válida.");
+                        break;
+                    case 2:
+                        System.out.println(grafo);
+                        break;
+                    case 3:
+                        System.out.println("Saliendo del programa.");
+                        break;
+                    default:
+                        System.out.println("Opción no válida.");
+                }
+            } catch (Exception e) {
+                System.out.println("Error: Ingrese datos numéricos");
+                entrada.next();
+                opcion = 0;
             }
-        } while (opcion != 4);
+        } while (opcion != 3);
     }
 
     public static Vertice crearVertice(Grafo grafo, Scanner entrada) {
@@ -58,22 +61,28 @@ public class Main {
         do {
             flag = false;
             System.out.println("Ingresa el valor del vertice");
-            int vertice = entrada.nextInt();
+            try {
+                int vertice = entrada.nextInt();
 
-            if (vertice % 14 == 0) { // Validación de múltiplo de 14
-                for (Vertice v : listaDeVertices) {
-                    if (v.getDato() == vertice) { // Validación para repetidos
-                        System.out.println("El vértice ya existe. Introduzca un valor diferente.");
-                        flag = true;
-                        break;
+                if (vertice % 14 == 0) { // Validación de múltiplo de 14
+                    for (int i = 0; i < listaDeVertices.size() && flag == false; i++) {
+                        if (listaDeVertices.get(i).getDato() == vertice) { // Validación para repetidos
+                            System.out.println("El vértice ya existe. Introduzca un valor diferente.");
+                            flag = true;
+                        }
                     }
-                }
 
-                if (!flag) {
-                    nuevoVertice = new Vertice(vertice);
+                    if (!flag) {
+                        nuevoVertice = new Vertice(vertice);
+                    }
+                } else {
+                    System.out.println("El valor debe ser múltiplo de 14.");
+                    flag = true;
                 }
-            } else {
-                System.out.println("El valor debe ser múltiplo de 14.");
+            } catch (Exception e) {
+                System.out.println("Error: Ingrese datos numéricos");
+                entrada.next();
+                flag = true;
             }
         } while (flag);
 
--- src/models/Grafo.java
+++ src/models/Grafo.java
@@ -13,20 +13,41 @@ public class Grafo {
     }
 
     public void agregarAristas() {
-        int opcion = 0;
-        int peso;
-        Vertice destino, origen;
+        int opcion = 1;
+        int peso = 0;
+        int opcionOrigen, opcionDestino;
+        Vertice destino = null;
+        Vertice origen = null;
         Scanner entrada = new Scanner(System.in);
+        boolean pesoValido = false;
+
         do {
+
             for (int i = 0; i < listaVertices.size(); i++) {
                 System.out.println((i + 1) + " )" + listaVertices.get(i).getDato());
             }
-            System.out.println("Seleccione el numero del vertice origen ");
-            int opcionOrigen = entrada.nextInt();
-            origen = listaVertices.get(opcionOrigen - 1);
-            System.out.println("Indique el numero del vertice destino ");
-            int opcionDestino = entrada.nextInt();
-            destino = listaVertices.get(opcionDestino - 1);
+
+            try {
+                System.out.println("Seleccione el número del vértice origen: ");
+                opcionOrigen = entrada.nextInt();
+                System.out.println("Indique el número del vértice destino: ");
+                opcionDestino = entrada.nextInt();
+
+                if (opcionOrigen == opcionDestino) {
+                    System.out.println("Los vertices deben ser distintos");
+                    continue;
+                }else if (opcionOrigen > listaVertices.size() || opcionDestino > listaVertices.size()){
+                    System.out.println("El numero que ingrese debe ser de una de las opciones: del 1 al " + listaVertices.size());
+                    continue;
+                }else {
+                    origen = listaVertices.get(opcionOrigen - 1);
+                    destino = listaVertices.get(opcionDestino - 1);
+                }
+
+            } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                System.out.println("Error: Ingrese un número válido.");
+                continue;
+            }
 
             if (existeArista(origen, destino)) {
                 System.out.println("La arista entre " + origen.getDato() + " y " + destino.getDato() + " ya existe.");
@@ -34,12 +55,22 @@ public class Grafo {
             }
 
             do {
-                System.out.println("Indique el peso ");
-                peso = entrada.nextInt();
-                if (peso <= 0) {
-                    System.out.println("Introduzca un peso positivo.");
+                pesoValido = false;
+                try{
+                    System.out.println("Indique el peso ");
+                    peso = entrada.nextInt();
+
+                    if (peso <= 0) {
+                        System.out.println("Introduzca un peso positivo.");
+                        pesoValido = false;
+                    }else {
+                        pesoValido = true;
+                    }
+                }catch (Exception e){
+                    System.out.println("Error: Ingrese datos numéricos");
+                    entrada.next();
                 }
-            } while (peso <= 0);
+            } while (pesoValido == false);
 
             origen.setAristas(new Arista(origen, destino, peso));
             System.out.println("Agregar mas aristas: 1)Si\t2)No");
